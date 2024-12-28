7d10ff2e89a95676d6a2a34118722f5c88b1cb16
meena76710 <149704835+meena76710@users.noreply.github.com>
2024-12-28 06:59:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+<html>
+  <head> Reach </head>
+  <body>
+    <div class="special">
+      <h2> this is javascript</h2>
+      <p> scripting language</p>
+
+    </div>
+    <div class="first">
+      <h1> TMf</h1>
+      <h2>Techm</h2>
+    </div>
+    <script>
+      let x="document"
+      document.getelementbyId(first);
+      consile.log(x)
+
+    </script>
+  </body>
+</html>
